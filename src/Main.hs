36195f57ae66be63029411287b587bae1a8eaea9
Seena Burns <hello@seenaburns.com>
2018-03-18 23:33:54 +0000

    Read spec string, data path from cli args
    For the first iteration, a protocol "spec" will be pulled from
    commandline args, e.g. "1 2 4 bytes" and the hex data from a given path

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import qualified System.Environment
+import qualified System.Exit
+import Data.Maybe
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  cliArgs <- System.Environment.getArgs
+  args <- return $ parseArgs cliArgs
+  case args of
+    Nothing -> usage
+    Just a -> putStrLn (show a)
+
+data Args = Args {
+  spec :: String,
+  dataPath :: FilePath
+} deriving (Show)
+
+-- Parse cli arguments
+parseArgs :: [String] -> Maybe Args
+parseArgs ("-h":_) = Nothing
+parseArgs ("--help":_) = Nothing
+parseArgs ("help":_) = Nothing
+parseArgs [specString,dataPath] = Just $ Args specString dataPath
+parseArgs _ = Nothing
+
+usage :: IO ()
+usage = putStrLn "Usage: protop [-h] \"spec string\" path/to/data" >> System.Exit.exitWith (System.Exit.ExitFailure 1)
